c33fe58f63e46bb7d074cb85ebe7853fb5003a36
sashazav-netizen <sashazav@gmail.com>
2026-03-18 19:42:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-const cacheName = 'budget-v1';
+const CACHE_NAME = 'budget-app-v1';
 const assets = ['./', './index.html', './manifest.json', './icon.png'];
 
+// התקנה ושמירת קבצים בסיסיים בזיכרון המטמון
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
 });
 
+// מאפשר לאפליקציה להיפתח גם כשאין אינטרנט
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
