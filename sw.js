a64f37b79681e97853ce5a967ef37c07fc481d6a
sashazav-netizen <sashazav@gmail.com>
2026-03-18 20:25:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
-const CACHE_NAME = 'budget-app-v1';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
-
-// התקנה ושמירת קבצים בסיסיים בזיכרון המטמון
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open('budget-store').then((cache) => cache.addAll([
+      'index.html',
+      'manifest.json'
+    ]))
   );
 });
 
-// מאפשר לאפליקציה להיפתח גם כשאין אינטרנט
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
